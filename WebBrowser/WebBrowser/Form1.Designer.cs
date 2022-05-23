27a2e2227e1f42479418793af6065849675067e4
Eray Gürbüz <eray58gurbuz@gmail.com>
2022-05-23 16:52:52 +0300

    C# Web Browser
    C# Web Browser

---
+++ WebBrowser/WebBrowser/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WebBrowser
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tsBtnBack = new System.Windows.Forms.ToolStripButton();
+            this.tsBtnForward = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsCbLocation = new System.Windows.Forms.ToolStripComboBox();
+            this.tsBtnGo = new System.Windows.Forms.ToolStripButton();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsBtnBack,
+            this.tsBtnForward,
+            this.toolStripSeparator1,
+            this.tsCbLocation,
+            this.tsBtnGo});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 28);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.ContextMenuStrip = this.contextMenuStrip1;
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 28);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(800, 422);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // tsBtnBack
+            // 
+            this.tsBtnBack.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsBtnBack.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsBtnBack.Name = "tsBtnBack";
+            this.tsBtnBack.Size = new System.Drawing.Size(44, 28);
+            this.tsBtnBack.Text = "Back";
+            this.tsBtnBack.Click += new System.EventHandler(this.tsBtnBack_Click);
+            // 
+            // tsBtnForward
+            // 
+            this.tsBtnForward.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsBtnForward.Image = ((System.Drawing.Image)(resources.GetObject("tsBtnForward.Image")));
+            this.tsBtnForward.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsBtnForward.Name = "tsBtnForward";
+            this.tsBtnForward.Size = new System.Drawing.Size(67, 25);
+            this.tsBtnForward.Text = "Forward";
+            this.tsBtnForward.Click += new System.EventHandler(this.tsBtnForward_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 31);
+            // 
+            // tsCbLocation
+            // 
+            this.tsCbLocation.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllUrl;
+            this.tsCbLocation.Name = "tsCbLocation";
+            this.tsCbLocation.Size = new System.Drawing.Size(121, 28);
+            this.tsCbLocation.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tsCbLocation_KeyDown);
+            // 
+            // tsBtnGo
+            // 
+            this.tsBtnGo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsBtnGo.Image = ((System.Drawing.Image)(resources.GetObject("tsBtnGo.Image")));
+            this.tsBtnGo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsBtnGo.Name = "tsBtnGo";
+            this.tsBtnGo.Size = new System.Drawing.Size(32, 28);
+            this.tsBtnGo.Text = "Go";
+            this.tsBtnGo.Click += new System.EventHandler(this.tsBtnGo_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "frmMain";
+            this.Text = "G Browser";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsBtnBack;
+        private System.Windows.Forms.ToolStripButton tsBtnForward;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripComboBox tsCbLocation;
+        private System.Windows.Forms.ToolStripButton tsBtnGo;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ WebBrowser/WebBrowser/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void tsCbLocation_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Enter)
+            {
+                webBrowser1.Navigate(tsCbLocation.Text);
+            }
+        }
+
+        private void tsBtnBack_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void tsBtnForward_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void tsBtnGo_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(tsCbLocation.Text);
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            Text = "G Browser-" + webBrowser1.Document.Title;
+        }
+    }
+}
